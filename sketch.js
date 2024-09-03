da980074f91bbb9fee59ef7547249c407ecb108f
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-02 21:28:47 -0500

    FInished with HTML video tutorial

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 function setup() {
   //add your setup code here
-    createCanvas(400, 400); //Example: create a canvas with 
+    createCanvas(window.innerWidth); //Example: create a canvas with 
     //width and height of 400 pixels
   }
   
   function draw() {
     // Add your drawing code here
     background(220); // Example: set the background color to
+    circle(mouseX, mouseY, width*0.01);
   }
