6757b6dc14e2e86196615483f60d865599938880
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-02 20:50:35 -0500

    File Upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  //add your setup code here
+    createCanvas(400, 400); //Example: create a canvas with 
+    //width and height of 400 pixels
+  }
+  
+  function draw() {
+    // Add your drawing code here
+    background(220); // Example: set the background color to
+  }
