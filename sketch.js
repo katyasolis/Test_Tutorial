3fa7932da49a8bf0992b3e0fbc7822b3a5a9575e
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-05 10:58:56 -0500

    Some progress with css tutorial

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
   //add your setup code here
-    createCanvas(window.innerWidth); //Example: create a canvas with 
+    createCanvas(window.innerWidth, window.innerHeight); //Example: create a canvas with 
     //width and height of 400 pixels
   }
   
   function draw() {
     // Add your drawing code here
-    background(220); // Example: set the background color to
-    circle(mouseX, mouseY, width*0.01);
+    background(100, 150, 200); // Example: set the background color to
+    circle(mouseX, mouseY, width*0.05);
   }
