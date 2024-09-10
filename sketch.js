4a2ec9de50f89b417f1e8931a864ccaf962ba2a6
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-09-10 10:44:57 -0500

    help (cry)

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
+let canvas; // Declare the variable for the canvas
+let makeBig = false; // Declare the variable for the button
+
 function setup() {
   //add your setup code here
-    createCanvas(window.innerWidth, window.innerHeight); //Example: create a canvas with 
+    canvas = createCanvas(window.innerWidth, window.innerHeight); //Example: create a canvas with 
     //width and height of 400 pixels
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
   }
   
   function draw() {
@@ -9,3 +14,29 @@ function setup() {
     background(100, 150, 200); // Example: set the background color to
     circle(mouseX, mouseY, width*0.05);
   }
+
+  //REFERENCE HTML ELEMENTS
+  let mainImage = select("#main-image");
+  let headerTwo = select("#header-two");
+  
+  mainImage.mousePressed(() => {
+    makeBig = !makeBig;
+    if(makeBig) {
+      mainImage.style('width', '100%');
+    } else {
+      mainImage.style('width', '20%');
+    }
+  });
+
+  headerTwo.mouseOver(() => {
+    headerTwo.style('color', 'red');
+    headerTwo.style('font-size', '50px');
+  });
+
+  headerTwo.mouseOver(() => {
+    headerTwo.style('color', 'blue');
+    headerTwo.style('font-size', '30px');
+  });
+
+
+  
